1b6590ecee092f0bb28ef326904f311ad2f567a9
Ganesh23105 <ganeshpandhre2005@gmail.com>
2024-01-19 23:58:23 +0530

    ganesh_hi

--- 17_01_mini/Mini project/tempCodeRunnerFile.py
+++ 17_01_mini/Mini project/tempCodeRunnerFile.py
@@ -1,2 +1,5 @@
    # if college_id_entry.get()==''and college_name_entry.get()==''and college_place_entry.get()==''and college_address_entry.get()==''and college_email_entry.get()==''and college_admin_name_entry.get()==''and college_Admin_email_entry.get()==''and college_password_entry.get()==''and college_confirm_password_entry.get()=='':
    #      messagebox.showerror('Error','All fields are required.')
+
+
+# hi ganesh
